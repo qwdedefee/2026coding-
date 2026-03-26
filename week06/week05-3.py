951cb6e6bfde5ff2f07a4277ecdf820597e685f3
qwdedefee <gary23381505@gmail.com>
2026-03-26 16:10:59 +0800

    Delete week06 directory

--- week06/week05-3.py
+++
@@ -1,9 +0,0 @@
-##week05-3.py leetcode 1207
-class Solution:
-    def uniqueOccurrences(self, arr: List[int]) -> bool:
-        c=Counter(arr)
-        s=set()
-        for i in c:
-            if c[i] in s: return False
-            s.add(c[i])
-        return True
